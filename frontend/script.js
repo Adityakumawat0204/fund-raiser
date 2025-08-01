5d2ad2cc47323c54dff1c836ce5b74736792355b
Aditya kumawat <142652660+Adityakumawat0204@users.noreply.github.com>
2025-08-01 21:26:54 +0530

    Update script.js

--- frontend/script.js
+++ frontend/script.js
@@ -1,3 +1,3 @@
 const API_BASE = window.location.origin.includes("localhost")
   ? "http://localhost:5000"
-  : "https://your-backend-name.onrender.com";
+: "https://fund-raiser-backend.onrender.com"
